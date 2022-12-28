15b9687aad58dd755231b1fe0040216b4b151fbc
dariusjecu <dariusjecu@yahoo.com>
2022-12-28 17:46:09 +0000

    New

--- src/data.jsx
+++ src/data.jsx
@@ -1,3 +1,14 @@
+import Img1 from "../public/home-images-test/img1.jpeg"
+import Img2 from "../public/home-images-test/img2.jpeg"
+import Img3 from "../public/home-images-test/img3.jpeg"
+import Img4 from "../public/home-images-test/img4.jpeg"
+import Img5 from "../public/home-images-test/img5.jpeg"
+import Img6 from "../public/home-images-test/img6.jpeg"
+import Img7 from "../public/home-images-test/img7.jpeg"
+import Img8 from "../public/home-images-test/img8.jpeg"
+import Img9 from "../public/home-images-test/img9.jpeg"
+import Img10 from "../public/home-images-test/img10.jpeg"
+
 export default [
     {
         photos: [
@@ -5,70 +16,70 @@ export default [
                 id: 1,
                 photographer: "Ceva",
                 src: {
-                    medium: "../public/home-images-test/img1.jpeg"
+                    medium: Img1
                 }
             },
             {
                 id: 2,
                 photographer: "Ceva",
                 src: {
-                    medium: "../public/home-images-test/img2.jpeg"
+                    medium: Img2
                 }
             },
             {
                 id: 3,
                 photographer: "Ceva",
                 src: {
-                    medium: "../public/home-images-test/img3.jpeg"
+                    medium: Img3
                 }
             },
             {
                 id: 4,
                 photographer: "Ceva",
                 src: {
-                    medium: "../public/home-images-test/img4.jpeg"
+                    medium: Img4
                 }
             },
             {
                 id: 5,
                 photographer: "Ceva",
                 src: {
-                    medium: "../public/home-images-test/img5.jpeg"
+                    medium: Img5
                 }
             },
             {
                 id: 6,
                 photographer: "Ceva",
                 src: {
-                    medium: "../public/home-images-test/img6.jpeg"
+                    medium: Img6
                 }
             },
             {
                 id: 7,
                 photographer: "Ceva",
                 src: {
-                    medium: "../public/home-images-test/img7.jpeg"
+                    medium: Img7
                 }
             },
             {
                 id: 8,
                 photographer: "Ceva",
                 src: {
-                    medium: "../public/home-images-test/img8.jpeg"
+                    medium: Img8
                 }
             },
             {
                 id: 9,
                 photographer: "Ceva",
                 src: {
-                    medium: "../public/home-images-test/img9.jpeg"
+                    medium: Img9
                 }
             },
             {
                 id: 10,
                 photographer: "Ceva",
                 src: {
-                    medium: "../public/home-images-test/img10.jpeg"
+                    medium: Img10
                 }
             }
         ]
